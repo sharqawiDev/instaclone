5f29a5c7c38b366a185a25276d279421baa082e8
sharqawidev <sharqawidev@gmail.com>
2021-05-05 22:01:48 +0300

    removing bottom tab label

--- components/Main.js
+++ components/Main.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialIcons from "react-native-vector-icons/MaterialCommunityIcons"
 import { fetchUser } from "../redux/actions"
 import FeedScreen from './main/Feed';
 import ProfileScreen from './main/Profile';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const Empty = () => null
 
@@ -24,18 +24,18 @@ class Main extends Component {
         )
         return (
 
-            <Tab.Navigator initialRouteName="Feed">
+            <Tab.Navigator initialRouteName="Feed" labeled={false}>
                 <Tab.Screen name="Feed" component={FeedScreen}
                     options={{
                         tabBarIcon: ({ color, size }) => (
-                            <MaterialIcons name="home" color={color} size={size} />
+                            <MaterialIcons name="home" color={color} size={26} />
                         ),
                     }}
                 />
                 <Tab.Screen name="AddContainer" component={Empty}
                     options={{
                         tabBarIcon: ({ color, size }) => (
-                            <MaterialIcons name="plus" color={color} size={size} />
+                            <MaterialIcons name="plus" color={color} size={26} />
                         ),
                         title: "Add"
                     }}
@@ -49,7 +49,7 @@ class Main extends Component {
                 <Tab.Screen name="Profile" component={ProfileScreen}
                     options={{
                         tabBarIcon: ({ color, size }) => (
-                            <MaterialIcons name="account" color={color} size={size} />
+                            <MaterialIcons name="account" color={color} size={26} />
                         ),
                     }}
                 />
