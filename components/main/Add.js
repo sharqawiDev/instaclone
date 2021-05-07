4867e034af1fdd249795eec77d67a638304863c2
sharqawidev <sharqawidev@gmail.com>
2021-05-07 20:56:25 +0300

    implementing image capture

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,90 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
-export default class Add extends Component {
-    render() {
-        return (
-            <View>
-                <Text> Add </Text>
-            </View>
-        )
+
+export default function App() {
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const [camera, setCamera] = useState(null);
+    const [image, setImage] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted')
+        })();
+    }, []);
+
+    const takePic = async () => {
+        if (camera) {
+            const data = await camera.takePictureAsync(null)
+            setImage(data.uri)
+        }
     }
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    if (hasCameraPermission === null || hasGalleryPermission === null) {
+        return <View />;
+    }
+    if (hasCameraPermission === false || hasGalleryPermission === false) {
+        return <Text>No access to camera or photos</Text>;
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.cameraContainer}>
+                <Camera ref={ref => setCamera(ref)} style={styles.fixedRatio} type={type} ratio={"1:1"} />
+            </View>
+            <Button
+                title={"Flip"}
+                onPress={() => {
+                    setType(
+                        type === Camera.Constants.Type.back
+                            ? Camera.Constants.Type.front
+                            : Camera.Constants.Type.back
+                    );
+                }} />
+
+            <Button title="Shoot" onPress={() => takePic()} />
+            <Button title="Pick" onPress={() => pickImage()} />
+            {image && <Image source={{ uri: image }} style={styles.image} />}
+
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    cameraContainer: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    fixedRatio: {
+        flex: 1,
+        aspectRatio: 1
+    },
+    image: {
+        flex: 1
+    }
+});
--- components/main/Feed.js
+++ components/main/Feed.js
@@ -4,7 +4,11 @@ import { Text, View } from 'react-native'
 export default class Feed extends Component {
     render() {
         return (
-            <View>
+            <View style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
                 <Text> Feed </Text>
             </View>
         )
